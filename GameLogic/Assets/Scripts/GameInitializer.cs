628bc026b7f853202a4f780357c5ba7a1511cec6
aftah <abdelfetah.hamra@skynet.be>
2019-06-12 09:11:49 +0200

    Game iniatizer Game Manager Team Manager

---
+++ GameLogic/Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameInitializer : MonoBehaviour
+{
+    private void Awake()
+    {
+
+
+        //Todo: FindObjectOfType<GameInitializer>().onMenu += OnMenuListernerHandler; 
+        //Todo: Intercepté l event du Menu Start
+    }
+    public class ModeEventArg : EventArgs
+    {
+        public int Mode;
+
+    }
+
+    public class MapEventArg : EventArgs
+    {
+        public int Map;
+    }
+
+
+    public class UIEventArg
+    {
+
+    }
+
+    public class TeamEventArg : EventArgs
+    {
+        public List<int> Team1;
+        public List<int> Team2;
+    }
+
+    public EventHandler<ModeEventArg> onMode;
+    public EventHandler<MapEventArg> onMap;
+    public EventHandler<TeamEventArg> onTeam;
+    public EventHandler<UIEventArg> onUI;
+
+
+
+    private void OnMode(ModeEventArg e)
+    {
+
+        if (onMode != null)
+        {
+            onMode(this, e);
+        }
+    }
+
+    private void OnMap(MapEventArg e)
+    {
+
+        if (onMap != null)
+        {
+            onMap(this, e);
+        }
+    }
+
+
+
+    private void OnTeam(TeamEventArg e)
+    {
+        if (onTeam != null)
+        {
+
+            onTeam(this, e);
+        }
+
+    }
+
+    private void Mode()
+    {
+
+        OnMode(new ModeEventArg() { });
+    }
+
+}
---
+++ GameLogic/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    //Creation Variables
+
+    private void Awake()
+    {
+        FindObjectOfType<GameInitializer>().onMode += OnModeListenerHandler;
+
+    }
+    public class StartEventArg : EventArgs 
+    {
+    }
+
+    public class StopEventArg : EventArgs 
+    {
+    }
+
+    public class CharacterEventArg : EventArgs
+    {
+
+
+    }
+    public class PositionEventArg : EventArgs 
+    {
+    }
+
+    public EventHandler<StartEventArg> onStart;
+    public EventHandler<StopEventArg> onStop;
+    public EventHandler<CharacterEventArg> onCharacter;
+    public EventHandler<PositionEventArg> onPosition;
+
+   
+
+    private void OnModeListenerHandler(object sender, GameInitializer.ModeEventArg e)
+    {
+        //initialisation du mode de jeux
+    }
+
+    private void OnStart(StartEventArg e)
+    {
+        if (onStart != null)
+        {
+            onStart(this, e);
+        }
+    }
+
+    private void OnStop(StopEventArg e)
+    {
+        if (onStop != null)
+        {
+            onStop(this, e);
+        }
+    }
+    private void OnCharacter(CharacterEventArg e)
+    {
+
+        if (onCharacter != null)
+        {
+            onCharacter(this, e);
+        }
+    }
+
+    private void OnPosition(PositionEventArg e)
+    {
+
+        if(onPosition != null)
+        {
+            onPosition(this, e);
+        }
+    }
+}
+
